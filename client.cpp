5802f1410fbce1f54dad9ba1236c88ba4f9e4660
yuruohan <1005623825@qq.com>
2020-12-18 16:46:22 +0800

    client frame

---
+++ client.cpp
@@ -0,0 +1,166 @@
+#include "client.h"
+
+int main()
+{
+    myClient client;
+    client.run();
+}
+
+myClient::myClient()
+{
+    cout << "[ debug] in myClient::myClinet\n";
+    socketfd = -1;
+}
+
+myClient::~myClient()
+{
+
+}
+
+void myClient::init()
+{
+    cout << "[ debug] socket()...\n";
+    socketfd = socket(F_INET, SOCK_STREAM, 0);
+    cout << "[ debug] socketfd = " << socketfd << endl;
+    printMenu();
+}
+
+void myClient::run()
+{
+    cout << "[ debug] in void myClient::run()\n";
+    init();
+    while(true)
+    {
+        string command;
+        vector<string> words;
+
+        cout << ">> ";
+        
+        getline(cin, command);
+        cout << "[ debug] your command is: " << command << endl;
+        /*
+         *  TODO:
+         *  把command划分成由空格断开的多个短字，并存放在words里
+         *  暂时使用参考代码里已实现好的
+         */
+
+        // split begins
+            regex whitespace("\\s+");
+            vector<string> words(sregex_token_iterator(command.begin(), command.end(), whitespace, -1),
+                             sregex_token_iterator());
+        // split finished
+
+        string op = words[0];
+        if (op == "")
+        {
+            continue;
+        }
+        else if (op =="connect")
+        {
+            cout << "[ debug] you want to connect\n";
+            if (-1 != socketfd)
+            {
+                /* TODO
+                 * 从serverAddr中获得
+                 *  sin_addr.s_addr -ip地址
+                 *  sin_port        -端口号
+                 * 并通过ntohl函数转换成string，然后打印
+                 */
+                cout << "Already connected to \n";
+            }
+            else
+            {
+                // connect
+                serverAddr.sin_family = AF_INET;
+                serverAddr.sin_port = htons(stoi(words[2]));
+                serverAddr.sin_addr.s_addr = inet_addr(words[1].c_str());
+
+                cout << "[ debug] connect()...\n";
+                if( connect(sockfd, (sockaddr*)&serverAddr, (socklen_t)sizeof(serverAddr)) < 0 )
+                {
+                    cout << "Connect failed!\n";
+                }
+                cout << "[ debug] pthread_create()...\n";
+                // new thread: 新线程的内存单元，null指针，新线程开始的地方，传入参数给start_rtn
+                pthread_create(&tidp, nullptr, start_rtn, &sockfd);
+            }
+            
+            
+        }
+        else if (op == "gettime")
+        {
+            // 调用函数send
+            cout << "404 not done. \n";
+        }
+        else if (op == "getname")
+        {
+            // 调用函数send
+            cout << "404 not done. \n";
+        }
+        else if (op == "getclients")
+        {
+            // 调用函数send
+            cout << "404 not done. \n";
+        }
+        else if (op == "send")
+        {
+            // 调用函数send
+            cout << "404 not done. \n";
+        }
+        else if (op == "close")
+        {
+            cout << "[ debug] you want to close\n";
+            if(-1 == socketfd)
+            {
+                cout << "No connection.\n";
+            }
+            else
+            {
+                cout << "[ debug] disconnect()...\n";
+                disconnect();
+                // TODO: 发送disconnected包
+                //       关闭子线程
+                // 考虑把close放进一个函数里面。
+                // socketfd = -1;
+                // cout << "Connection closed. \n";
+            }
+        }
+        else if (op == "exit")
+        {
+            if(-1 != socketfd)
+            {
+                disconnect();
+            }
+            cout << "Exit Clinet\n";
+            exit(0);
+        }
+        else if (op == "help")
+        {
+            printMenu();
+        }
+        else
+        {
+            cout << "Do not find operation: " << op << "\n";
+        }
+    }
+}
+
+// 新创建的子进程要做什么？
+void connection_handle(int sfd)
+{
+    // 创建时，第一次connect成功，应该print提示消息
+    // TODO: keep receiving
+}
+
+void printMenu()
+{
+    cout << "Please input a command:\n"
+         << "- connect [IP] [port]\n"
+         << "- gettime\n"
+         << "- getname\n"
+         << "- getclients\n"
+         << "- send [IP] [port] [message]\n"
+         << "- close\n"
+         << "- exit\n"
+         << "- help\n";
+}
--- client.h
+++ client.h
@@ -3,28 +3,49 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <regex>
 
-#include <pthread.h>
+#include <pthread.h>    // 多线程
 
-#include <sys/socket.h>
+#include <sys/socket.h> // socket基本接口
+#include <arpa/inet.h>  // htonl，htons，ntohl，ntohs等字节序转换函数
+
+using namespace std;
+
+// TODO
+#define BUFFER_SIZE 256
+void connection_handle(int sfd);
+
+class myClient
+{
+private:
+    int socketfd;               // 客户端的socket描述字
+    sockaddr_in serverAddr;     // 所连接的服务器的socket地址
+    // Note:
+    //  sin_family:对应的是地址类型：AF_INET代表ipv4。
+    //  sin_port:代表端口号。
+    //  sin_addr.s_addr:代表我们所建立的ip地址。
+    
+    pthread_t tidp;             // 新创建的线程ID指向的内存单元
+
+    void init();    // 通过socket获得socketfd，打印菜单
+    void disconnect();  // 断开连接，发送
+    void printMenu();
+
+    // TODO: why static?
+    static void * start_rtn(void * sfd) // 新创建的线程从start_rtn函数的地址开始运行
+    {
+        connection_handle(*(int*) sfd); // TODO
+        return nullptr;
+    }
 
 
+public:
+    myClient(); // 初始化一些类成员变量
+    ~myClient();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    void run(); // 对输入命令进行响应
+};
 
 #endif
